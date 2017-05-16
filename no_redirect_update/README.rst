594f8924634fd5c9890336421fa699041770de02
Reda ROUICHI <r2.red.a@gmail.com>
2017-05-16 13:59:12 +0100

    [init]

---
+++ no_redirect_update/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=====================================
+Avoid redirect when updating a module
+=====================================
+
+This module lets you avoid redirect when updating a module in Odoo 10.
---
+++ no_redirect_update/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Avoid redirect when updating a module",
+    'summary': """
+        Avoid redirect when updating a module in Odoo 10""",
+    "author"      : "Karizma Conseil",
+    'company': 'Karizma Conseil',
+    "website"     : "http://www.karizma.ma",
+    'category': 'Tools',
+    'version': '0.2',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+}
